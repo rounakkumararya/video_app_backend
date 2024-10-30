6c58639a739bcc354b271a322d6ee78bccb63b46
Rounak Kumar Arya <rounakkumar040@gmail.com>
2024-10-30 21:19:10 +0530

    add user and video model

---
+++ src/models/video.model.js
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      required: true,
+      type: String,
+    },
+    thumbnail: {
+      required: true,
+      type: String,
+    },
+    title: {
+      required: true,
+      type: String,
+    },
+    description: {
+      required: true,
+      type: String,
+    },
+    duration: {
+      required: true,
+      type: Number,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types,
+    },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", videoSchema);
