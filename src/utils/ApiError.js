2e978683c5b3e87717a182f697100a9e6c017739
Rounak Kumar Arya <rounakkumar040@gmail.com>
2024-10-30 16:08:06 +0530

    utils code

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -11,5 +11,13 @@ class ApiError extends Error {
     this.message = message;
     this.success = false;
     this.errors = errors;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
   }
 }
+
+export { ApiError };
