f93fbf91d012c831af61879959e0cb4d8f42271a
Rounak Kumar Arya <rounakkumar040@gmail.com>
2024-11-05 21:49:54 +0530

    create logout user and auth middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -139,7 +139,32 @@ const loginUser = asyncHandler(async (req, res) => {
 
 })
 
+const logoutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id, {
+        $set: {
+            refreshToken: undefined
+        }
+    }, {
+        new: true
+    }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200).clearCookie("accessToken").clearCookie("refreshToken").json(
+        new ApiResponse(200, {}, "User logged out")
+    )
+
+
+})
+
+
 export {
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
-import {upload} from "../middlewares/multer.middleware.js"
+import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js"
+import {verifyJWT} from '../middlewares/auth.middleware.js'
 
 
 const router = Router()
@@ -10,14 +11,19 @@ router.route("/register").post(
         {
             name: "avatar",
             maxCount: 1
-        }, 
+        },
         {
             name: "coverImage",
             maxCount: 1
         }
     ]),
     registerUser
-    )
+)
+
+router.route("/login").post(loginUser)
+
+
+router.route("/login").post(verifyJWT, logoutUser)
 
 
 export default router
